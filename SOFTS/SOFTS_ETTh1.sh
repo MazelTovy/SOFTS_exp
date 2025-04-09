7f2e31982fa2b364e9e554220a37ec7514b49bae
MazelTovy <2548848618@qq.com>
2025-04-09 00:26:00 -0400

    compare

---
+++ SOFTS/SOFTS_ETTh1.sh
@@ -0,0 +1,32 @@
+model_name=SOFTS
+seq_len=48
+
+for pred_len in 48 72 96 144 192
+do
+  python -u run.py \
+    --is_training 1 \
+    --root_path ./dataset/ETT/ \
+    --data_path ETTm2.csv \
+    --model_id 'ETTm2_'$seq_len'_'$pred_len \
+    --model $model_name \
+    --data ETTm2 \
+    --features M \
+    --seq_len $seq_len \
+    --pred_len $pred_len \
+    --e_layers 1 \
+    --enc_in 7 \
+    --dec_in 7 \
+    --c_out 7 \
+    --d_model 256 \
+    --d_core 256 \
+    --d_ff 256 \
+    --learning_rate 0.0003 \
+    --lradj cosine \
+    --train_epochs 20 \
+    --patience 3 \
+    --des 'Exp' \
+    --itr 1 \
+    --lambda_penalty 1 \
+    --sim_threshold 0.7 \
+    --dist_threshold 0.5
+done
